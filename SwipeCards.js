1f9ab9bbe13c9295b78313ba7a5a85681733e873
Eyal Abs <eyalyoli@gmail.com>
2020-12-28 12:15:54 +0200

    fix bug when no keyExtractor prop

--- SwipeCards.js
+++ SwipeCards.js
@@ -432,7 +432,11 @@ export default class SwipeCards extends Component {
 
         return (
           <Animated.View
-            key={this.props.keyExtractor(card)}
+            key={
+              this.props.keyExtractor
+                ? this.props.keyExtractor(card)
+                : String(i)
+            }
             style={[styles.card, animatedCardStyles]}
             {...this._panResponder.panHandlers}
           >
@@ -442,7 +446,12 @@ export default class SwipeCards extends Component {
       }
 
       return (
-        <Animated.View key={this.props.keyExtractor(card)} style={style}>
+        <Animated.View
+          key={
+            this.props.keyExtractor ? this.props.keyExtractor(card) : String(i)
+          }
+          style={style}
+        >
           {this.props.renderCard(card)}
         </Animated.View>
       );
@@ -475,7 +484,11 @@ export default class SwipeCards extends Component {
 
     return (
       <Animated.View
-        key={this.props.keyExtractor(this.state.card)}
+        key={
+          this.props.keyExtractor
+            ? this.props.keyExtractor(this.state.card)
+            : undefined
+        }
         style={[styles.card, animatedCardStyles]}
         {...this._panResponder.panHandlers}
       >
