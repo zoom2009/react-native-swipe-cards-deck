1be4daa104a2a6b1ec89d561c4121698210fb76c
Eyal Abs <eyal@swaplet.app>
2021-05-24 00:39:09 +0300

    extract styles

--- SwipeCards.js
+++ SwipeCards.js
@@ -15,57 +15,9 @@ import {
 
 import Defaults from "./Defaults.js";
 import clamp from "clamp";
+import { styles } from "./styles";
 
-const viewport = Dimensions.get("window");
-const SWIPE_THRESHOLD = 120;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "transparent",
-  },
-  yup: {
-    borderColor: "green",
-    borderWidth: 2,
-    position: "absolute",
-    padding: 20,
-    bottom: 20,
-    borderRadius: 5,
-    right: 0,
-  },
-  yupText: {
-    fontSize: 16,
-    color: "green",
-  },
-  maybe: {
-    borderColor: "blue",
-    borderWidth: 2,
-    position: "absolute",
-    padding: 20,
-    bottom: 20,
-    borderRadius: 5,
-    right: 20,
-  },
-  maybeText: {
-    fontSize: 16,
-    color: "blue",
-  },
-  nope: {
-    borderColor: "red",
-    borderWidth: 2,
-    position: "absolute",
-    bottom: 20,
-    padding: 20,
-    borderRadius: 5,
-    left: 0,
-  },
-  nopeText: {
-    fontSize: 16,
-    color: "red",
-  },
-});
+const SWIPE_THRESHOLD = 120; //TODO threshold of action sensitivity
 
 //Components could be unloaded and loaded and we will loose the users currentIndex, we can persist it here.
 let currentIndex = {};
