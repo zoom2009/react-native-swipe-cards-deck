e85b0e2416a2ea47584c8d887f300f87c947a0a3
Eyal Abs <eyal@swaplet.app>
2021-05-24 00:40:46 +0300

    format

--- Defaults.js
+++ Defaults.js
@@ -1,13 +1,7 @@
-'use strict';
+"use strict";
 
-import React, {Component} from 'react';
-
-import {
-    StyleSheet,
-    Text,
-    View,
-    Image
-} from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 class NoMoreCards extends Component {
   constructor(props) {
@@ -19,16 +13,16 @@ class NoMoreCards extends Component {
       <View>
         <Text style={styles.noMoreCardsText}>No more cards</Text>
       </View>
-    )
+    );
   }
 }
 
 export default {
-  NoMoreCards: NoMoreCards
-}
+  NoMoreCards: NoMoreCards,
+};
 
 const styles = StyleSheet.create({
   noMoreCardsText: {
     fontSize: 22,
-  }
-})
+  },
+});
